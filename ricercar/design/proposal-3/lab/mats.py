4d99c1fba34ff402b0ee69d07a633d2787e5fd66
Boris Djordjevic <dboris@gmail.com>
2026-09-24 19:39:16 +0100

    Ignore Python bytecode; proposal-3 adds B-flat major forms of S1, S2 and the full theme for the apotheosis

--- ricercar/design/proposal-3/lab/mats.py
+++ ricercar/design/proposal-3/lab/mats.py
@@ -3,3 +3,6 @@ S1   = parse("bes'2. bes'8 a' | bes'2. bes'8 a' | bes'4. c''8 c''4. ees''8 | ees
 CS1  = parse("ees'2 d'2 | des'2 c'2 | bes2 aes2 | ges2 f2 | bes2")
 S1i  = parse("f'2. f'8 ges' | f'2. f'8 ges' | f'4. ees'8 ees'4. c'8 | c'2. des'8 ees' | f'2")
 S2   = parse("c''4. a'8 f'4 des''8 bes' | c''2. f''8 bes' | ees''4. des''8 des''4. c''8 | c''2")
+S1M  = parse("bes'2. bes'8 a' | bes'2. bes'8 a' | bes'4. c''8 c''4. ees''8 | ees''2. d''8 c'' | bes'2")
+THM  = parse("bes'2. bes'8 a' | bes'2. bes'8 a' | bes'4. c''8 c''4. ees''8 | ees''2. d''8 bes' | c''4. a'8 f'4 d''8 bes' | c''2. f''8 bes' | ees''4. d''8 d''4. c''8 | c''1")
+S2M  = parse("c''4. a'8 f'4 d''8 bes' | c''2. f''8 bes' | ees''4. d''8 d''4. c''8 | c''2")
