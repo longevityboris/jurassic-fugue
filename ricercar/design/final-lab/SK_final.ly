215efa6fc2405957a5d117e985b593d43c78891d
Boris Djordjevic <dboris@gmail.com>
2026-09-25 01:05:26 +0100

    Ricercar apotheosis (critique: thinnest, most static passage, film-cue I-IV6/4-I, hollow unisons, no real cadence): the whole tune now sounds over the exposition's triple counterpoint in B-flat major (CS1 lament in the bass with its A-flat and G-flat as the one minor shadow, CS2 in the tenor), then over the answer; the alto carries the thirds (no four-octave F at the tune's peak), 62:1 is a complete C7/B-flat and 62:4.5 a complete V7, so c''-d'' at 63 is a real cadence; the coda moves under the held d'' (inversion head in major, the subject's head last) over a re-struck tonic pedal, without the minor iv6/4. Checker and strict clean

--- ricercar/design/final-lab/SK_final.ly
+++ ricercar/design/final-lab/SK_final.ly
@@ -64,11 +64,11 @@ alto = \absolute {
   % 49
   c''2. f''8 bes'8 | ees''4. des''8 des''4. c''8 | c''1 | e''4. e''16 dis''16 e''2 |
   % 53
-  e''4. e''16 d''16 e''4 f''4 | c''2 a'4 c'4 | d'4. ees'8 f'4 ees'4 | d'4 f'4 ees'4 f'8 ees'8 |
+  e''4. e''16 d''16 e''4 f''4 | c''2 a'4 c'4 | d'1 | f'2. g'4 |
   % 57
-  f'4. ees'8 ees'4. f'8 | a'2 g'4 f'4~ | f'4 e'4 d'4 e'4~ | e'4 ees'4 f'2 |
+  g'4 f'2 ees'4 | bes'2 c''4 bes'8 f'8 | a'4. f'8 c'4 d'8 c'8 | f'2 a'4. g'8 |
   % 61
-  c''2. bes'4~ | bes'4 a'4 g'2 | f'1 | ges'2 f'2 |
+  a'4. bes'8 bes'4. g'8 | e'2. a'4 | f'2. f'8 g'8 | f'4 ees'4 d'2 |
   % 65
   f'2 ees'2 | d'2 f'2 |
 }
@@ -100,11 +100,11 @@ tenor = \absolute {
   % 49
   bes2. bes8 a8 | bes4. c'8 c'4. ees'8 | ees'2. des'8 bes8 | c'4. a8 bes2 |
   % 53
-  g2 g4 c'4 | a2 c'4 ees4 | f2 g4 f4 | bes4 aes4 g4 f4 |
+  g2 g4 c'4 | a2 c'4 ees4 | f1 | bes8 a8 bes8 c'8 d'8 c'8 ees'8 g8 |
   % 57
-  f4 d'4 c'4. c'8 | c'2. f4 | f2. f8 g8 | f2. f8 g8 |
+  d4 c8 f8 ~ f2 | ees8 f8 ees8 d8 c4 d4 | ees4 c4 a4 bes8 g8 | a2 c'4. bes8 |
   % 61
-  f4. e8 e4. c8 | c2. d8 f8 | d'1 | ees'2 d'2 |
+  f4. e8 e4. c8 | c2. ees4 | d2 f2 | bes2 f2 |
   % 65
   bes2. bes8 a8 | bes1 |
 }
@@ -136,11 +136,11 @@ bass = \absolute {
   % 49
   f,2 f,4 ges,4 | f,2. ees,4 | ees,2. c,4 | c,1 |
   % 53
-  c,2 des,4 f,4 | ees,2. ges,4 | bes,2. bes,8 c8 | bes,2. bes,8 c8 |
+  c,2 des,4 f,4 | ees,2. ges,4 | bes,2. f,4 | d2. c4 |
   % 57
-  bes,4. a,8 a,4. f,8 | f,2. d,4 | f,2. f,8 e,8 | f,2. f,8 e,8 |
+  bes,4 a,4 aes,2 | ges,2. f,4 | f,2. f,8 e,8 | f,2. f,8 e,8 |
   % 61
-  f,4. g,8 g,4. bes,8 | bes,2. a,8 f,8 | bes,1~ | bes,1 |
+  f,4. g,8 g,4. bes,8 | bes,2. a,8 f,8 | bes,1 | bes,1 |
   % 65
-  bes,1~ | bes,1 |
+  bes,1 | bes,1 |
 }
--- ricercar/design/final-lab/piece.py
+++ ricercar/design/final-lab/piece.py
@@ -144,19 +144,19 @@ SECTIONS = [
          breaths=[{'at': '1:1', 'ms': 250}],
          pedal=[],
     ),
-    dict(id='sec07_apotheosis_coda', title='Apotheosis (cantus firmus) and coda', bars=12,
-         soprano='''bes'2. bes'8 a'8 | bes'2. bes'8 a'8 | bes'4. c''8 c''4. ees''8 | ees''2. d''8 bes'8 |
+    dict(id='sec07_apotheosis_coda', title='Apotheosis: the whole tune over the triple counterpoint in B-flat major; coda', bars=12,
+         soprano="""bes'2. bes'8 a'8 | bes'2. bes'8 a'8 | bes'4. c''8 c''4. ees''8 | ees''2. d''8 bes'8 |
             c''4. a'8 f'4 d''8 bes'8 | c''2. f''8 bes'8 | ees''4. d''8 d''4. c''8 | c''1 |
-            d''1~ | d''1 | d''2 c''2 | d''1''',
-         alto='''d'4. ees'8 f'4 ees'4 | d'4 f'4 ees'4 f'8 ees'8 | f'4. ees'8 ees'4. f'8 | a'2 g'4 f'4~ |
-            f'4 e'4 d'4 e'4~ | e'4 ees'4 f'2 | c''2. bes'4~ | bes'4 a'4 g'2 |
-            f'1 | ges'2 f'2 | f'2 ees'2 | d'2 f'2''',
-         tenor='''f2 g4 f4 | bes4 aes4 g4 f4 | f4 d'4 c'4. c'8 | c'2. f4 |
-            f2. f8 g8 | f2. f8 g8 | f4. e8 e4. c8 | c2. d8 f8 |
-            d'1 | ees'2 d'2 | bes2. bes8 a8 | bes1''',
-         bass='''bes,2. bes,8 c8 | bes,2. bes,8 c8 | bes,4. a,8 a,4. f,8 | f,2. d,4 |
+            d''1~ | d''1 | d''2 c''2 | d''1""",
+         alto="""d'1 | f'2. g'4 | g'4 f'2 ees'4 | bes'2 c''4 bes'8 f'8 |
+            a'4. f'8 c'4 d'8 c'8 | f'2 a'4. g'8 | a'4. bes'8 bes'4. g'8 | e'2. a'4 |
+            f'2. f'8 g'8 | f'4 ees'4 d'2 | f'2 ees'2 | d'2 f'2""",
+         tenor="""f1 | bes8 a8 bes8 c'8 d'8 c'8 ees'8 g8 | d4 c8 f8 ~ f2 | ees8 f8 ees8 d8 c4 d4 |
+            ees4 c4 a4 bes8 g8 | a2 c'4. bes8 | f4. e8 e4. c8 | c2. ees4 |
+            d2 f2 | bes2 f2 | bes2. bes8 a8 | bes1""",
+         bass="""bes,2. f,4 | d2. c4 | bes,4 a,4 aes,2 | ges,2. f,4 |
             f,2. f,8 e,8 | f,2. f,8 e,8 | f,4. g,8 g,4. bes,8 | bes,2. a,8 f,8 |
-            bes,1~ | bes,1 | bes,1~ | bes,1''',
+            bes,1 | bes,1 | bes,1 | bes,1""",
          roles=[('soprano', '1:1', '9:1', 'subject'),
                 ('bass', '1:1', '9:1', 'cs'),
                 ('tenor', '5:1', '9:1', 'cs'),
